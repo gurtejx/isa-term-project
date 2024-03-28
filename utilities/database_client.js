272419a208e49c9584145ca793f6e7d4cd70f1f6
Gurtej Malik <gurtejmalik@gmail.com>
2024-03-27 17:43:12 -0700

    Create DB connect using MongoDB and Mongoose

---
+++ utilities/database_client.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+/**
+ * Database client class to connect to MongoDB.
+ * Follows singleton design pattern.
+ */
+export class DatabaseClient {
+  constructor() {
+    if (!DatabaseClient.instance) {
+      this.connect();
+      DatabaseClient.instance = this;
+    }
+    return DatabaseClient.instance;
+  }
+
+  async connect() {
+    try {
+      const uri = process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/`;
+      await mongoose.connect(uri, {
+        dbName: "isa_term_proj",
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log("MongoDB connected successfully");
+    } catch (error) {
+      console.error("Error connecting to MongoDB:", error);
+    }
+  }
+}
+
+export default DatabaseClient;
