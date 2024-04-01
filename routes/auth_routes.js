dc208e561fb333770de398124b6bb63616b84336
Gurtej Malik <gurtejmalik@gmail.com>
2024-04-01 05:08:44 -0700

    Create password reset feature

--- routes/auth_routes.js
+++ routes/auth_routes.js
@@ -5,6 +5,9 @@ import express from 'express';
 import MongoService from "../utilities/mongo_service.js";
 import bcrypt from 'bcrypt';
 import dotenv from "dotenv";
+import notifier from "node-notifier";
+import nodemailer from "nodemailer";
+import Mailgen from "mailgen";
 import jwt from 'jsonwebtoken';
 dotenv.config();
 
@@ -85,6 +88,170 @@ router.get('/signin', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render("signup");
 });
+
+// Route for forgot password page
+router.get('/forgotPassword', (req, res) => {
+  res.render('forgotPassword'); 
+});
+
+// Route for handling submission of email for password reset
+router.post('/sendResetEmail', async (req, res) => {
+  const userEmail = req.body.email;
+
+  // Validate email
+  if (!userEmail) {
+    res.redirect('/forgotPassword');
+    return;
+  }
+
+  // Check if email exists in database
+  const user = await mongo.models.User.findOne({ email: userEmail });
+  if (!user) {
+    res.redirect('/forgotPassword');
+    return;
+  }
+  const firstname = user.firstname;
+
+  // Generate token for password reset
+  const secret = process.env.JWT_SECRET + user.password;
+  const payload = { 
+    email: user.email,
+    id: user._id 
+  };
+  const token = jwt.sign(payload, secret, { expiresIn: '15m' });
+  const resetLink = `http://localhost:8000/setNewPassword/${user._id}/${token}`;
+
+  // Send password reset email
+  try {
+    // Create a Nodemailer transporter
+  const config = {
+    service: process.env.EMAIL_SERVICE_PROVIDER,
+    auth: {
+      user: process.env.APP_EMAIL,
+      pass: process.env.APP_EMAIL_PASSWORD,
+    },
+  };
+  let transporter = nodemailer.createTransport(config);
+
+  let Mailgenerator = new Mailgen({
+    theme: "default",
+    product: {
+      name: "Mailgen",
+      link: "https://mailgen.js/",
+    },
+  });
+
+  let response = {
+    req: req,
+    body: {
+      name: firstname,
+      intro: `Voila! We got you covered!.`,
+      action: {
+        instructions: "To reset your password, click the button below:",
+        button: {
+          text: "Reset Password",
+          link: resetLink,
+        },
+      },
+      outro: `If you have any questions, feel free to contact us at ${process.env.APP_EMAIL}.`
+    },
+  };
+
+  let emailToBeSent = Mailgenerator.generate(response);
+
+  // Define the email options
+  let mailOptions = {
+    from: process.env.APP_EMAIL,
+    to: userEmail,
+    subject: "Reset Your Password",
+    html: emailToBeSent,
+  };
+    
+  // Send the email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      // Display a notification alert
+      notifier.notify({
+        title: "Alert",
+        message: "Failed to send email: " + error,
+      });
+      console.log(error);
+      res.redirect("/");
+    } else {
+      // Display a notification alert
+      notifier.notify({
+        title: "Alert",
+        message: "Email sent: " + info.response,
+      });
+      console.log("Email sent: " + info.response);
+      res.redirect("/");
+    }
+  });
+
+    res.redirect('/signin');
+  } catch (error) {
+    console.error(error);
+    res.redirect('/forgotPassword');
+  }
+});
+
+// Route for setting new password
+router.get('/setNewPassword/:userId/:token', async (req, res) => {
+  const { userId, token } = req.params;
+
+  // Verify token
+  try {
+    const user = await mongo.models.User.findById(userId);
+    if (!user) {
+      res.redirect('/forgotPassword');
+      return;
+    }
+
+    const secret = process.env.JWT_SECRET + user.password;
+    const decodedToken = jwt.verify(token, secret);
+
+    // Token is valid, render page for setting new password
+    res.render('setNewPassword', { userId, token });
+  } catch (error) {
+    console.error(error);
+    res.redirect('/pageDoesNotExist');
+  }
+});
+
+router.post("/updatePassword", async (req, res) => {
+  const newPassword = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  const userId = req.body.userId;
+
+  if (!newPassword || !confirmPassword) {
+    return res.redirect("/setNewPassword");
+  }
+
+  if (newPassword!== confirmPassword) {
+    return res.redirect("/setNewPassword");
+  }
+
+  try {
+    // Hash the new password
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    // Update the user's password in the database
+    await mongo.models.User.findOneAndUpdate(
+      { _id: userId }, 
+      { $set: { password: hashedPassword } }
+    );
+
+    // Display success message
+    console.log("Password updated successfully!");
+    res.redirect("/signin"); // Redirect to login page
+  } catch (error) {
+    console.error("Failed to update password:", error);
+    // Display error message
+    res.redirect("/setNewPassword?error=true");
+  }
+});
+
+
 
 
 export default router;
