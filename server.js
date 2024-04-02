c11a17ec0fb28ff28c0ffc5c208c91fb5dd64db5
David Nam <lgg4david@gmail.com>
2024-04-02 14:51:54 -0700

    .env file updated

--- server.js
+++ server.js
@@ -1,46 +1,48 @@
 /**
- * Server.js file: Set up and configure Express server, create DB connection, 
+ * Server.js file: Set up and configure Express server, create DB connection,
  * define routes, and start the server listening for incoming requests.
  */
 import express from 'express';
 import cors from 'cors';
 import MongoService from "./utilities/mongo_service.js";
 import authRoutes from "./routes/auth_routes.js";
-import { isLoggedIn, isAdmin } from './utilities/auth_middleware.js';
+import {isLoggedIn, isAdmin} from './utilities/auth_middleware.js';
 import path from 'path';
 import cookie_parser from 'cookie-parser';
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const mongo = new MongoService();
 const app = express();
+
 app.use(express.json());
 app.use(cookie_parser());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.set("views", path.join(path.resolve(), "views"));
 app.use(express.static(path.join(path.resolve(), "public")));
 
 const cors_options = {
-  origin: '*',
-  optionsSuccessStatus: 200 
+    origin: '*',
+    optionsSuccessStatus: 200
 }
 app.use(cors(cors_options));
 
 // Define routes
 app.get('/', isLoggedIn, (req, res) => {
-  const isLoggedIn = req.verified ? true : false;
-  res.render("landingPage", { isLoggedIn: isLoggedIn });
+    const isLoggedIn = req.verified ? true : false;
+    res.render("landingPage", {isLoggedIn: isLoggedIn});
 });
 
 app.use('/', authRoutes);
 
 app.get('/admin', isLoggedIn, isAdmin, (req, res) => {
-  res.render("admin");
+    res.render("admin");
 });
 
 // Start Express server
 const PORT = 8000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- utilities/database_client.js
+++ utilities/database_client.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 /**
@@ -7,25 +8,25 @@ dotenv.config();
  * Follows singleton design pattern.
  */
 export class DatabaseClient {
-  constructor() {
-    if (!DatabaseClient.instance) {
-      this.connect();
-      DatabaseClient.instance = this;
+    constructor() {
+        if (!DatabaseClient.instance) {
+            this.connect();
+            DatabaseClient.instance = this;
+        }
+        return DatabaseClient.instance;
     }
-    return DatabaseClient.instance;
-  }
 
-  async connect() {
-    try {
-      const uri = process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/`;
-      await mongoose.connect(uri, {
-        dbName: "isa_term_proj",
-      });
-      console.log("MongoDB connected successfully");
-    } catch (error) {
-      console.error("Error connecting to MongoDB:", error);
+    async connect() {
+        try {
+            const uri = process.env.MONGODB_URI || `mongodb://127.0.0.1:27017/`;
+            await mongoose.connect(uri, {
+                dbName: "isa_term_proj",
+            });
+            console.log("MongoDB connected successfully");
+        } catch (error) {
+            console.error("Error connecting to MongoDB:", error);
+        }
     }
-  }
 }
 
 export default DatabaseClient;
